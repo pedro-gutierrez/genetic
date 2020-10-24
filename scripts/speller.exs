be4feb500b534aa35b1ceb6c51a3916cc245c5a4
Pedro Gutiérrez <pedro.gutierrez@form3.tech>
2020-10-25 00:03:51 +0200

    added speller problem

---
+++ scripts/speller.exs
@@ -0,0 +1,30 @@
+defmodule Speller do
+  alias Genetic.Types.Chromosome
+  @behaviour Genetic.Problem
+
+  @impl true
+  def genotype() do
+    genes =
+      Stream.repeatedly(fn -> Enum.random(?a..?z) end)
+      |> Enum.take(34)
+
+    %Chromosome{genes: genes, size: 34}
+  end
+
+  @impl true
+  def fitness_function(chromosome) do
+    target = "supercalifragilisticexpialidocious"
+    guess = List.to_string(chromosome.genes)
+    String.bag_distance(target, guess)
+  end
+
+  @impl true
+  def terminate?(population) do
+    hd(population).fitness == 1
+  end
+end
+
+
+soln = Genetic.run(Speller)
+IO.write("\n")
+IO.inspect(soln)
